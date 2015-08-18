e36fee44bede941b485e25e60e5a17fc76d75c5d
cmis <cmis@cmis-hs20.(none)>
2015-08-18 09:53:29 +0700

    finished inputmath

--- InputMath/InputMath.java
+++ InputMath/InputMath.java
@@ -5,17 +5,25 @@ public class InputMath
    
     public static void main ( String[] args )
     {
-        System.out.println( "Please one number." );
+        float numa, numb;
+        System.out.println( "Please give me one number." );
         
         Scanner input = new Scanner( System.in );
-        String numa = input.nextLine();
+        numa = input.nextFloat();
         
-        System.out.println( "Please another number." );
-        
-       
-        String numb = input.nextLine();
-        
+        System.out.println( "Please give me another number." );
+        numb = input.nextFloat();
         
        
+        System.out.println( numa + " + " + numb + " = " + (numa + numb));
+        System.out.println( numb + " + " + numa + " = " + (numb + numa));
+        System.out.println( numa + " - " + numb + " = " + (numa - numb));
+        System.out.println( numb + " - " + numa + " = " + (numb - numa));
+        System.out.println( numa + " x " + numb + " = " + (numa * numb));
+        System.out.println( numb + " x " + numa + " = " + (numb * numa));
+        System.out.println( numa + " / " + numb + " = " + (numa / numb));
+        System.out.println( numb + " / " + numa + " = " + (numb / numa));
+        System.out.println( numa + " % " + numb + " = " + (numa % numb));
+        System.out.println( numb + " % " + numa + " = " + (numb % numa));
     }
     }
