e5b083e9481b35f4753337bbadf8b768c5eb5461
cmis <cmis@cmis-hs20.(none)>
2015-08-21 13:15:50 +0700

    worked more on P5

--- FundamentalsP5/FundamentalsP5.java
+++ FundamentalsP5/FundamentalsP5.java
@@ -1,11 +1,35 @@
 import java.util.Scanner;
 public class FundamentalsP5
 {
+    
     public static void main ( String argv[] )
     {
-        for ( int i = 1; i < 20; i++ )
-        {
-            System.out.println(i);
-        }
+       
+        for (1 == 1; {
+        String[] favMovies;
+        favMovies = new String[3];
+        
+        favMovies[0] = "Pulp Fiction";
+        favMovies[1] = "Jurrassic World";
+        favMovies[2] = "Home" ;
+        System.out.println("My favorite movies are:");
+        System.out.println( favMovies[0] );
+        System.out.println( favMovies[1] );
+        System.out.println( favMovies[2] );
     }
-}
+    
+    while (2 ==2); {
+        String[] favSongs;
+        favSongs = new String[3];
+        
+        favSongs[0] = "Jesus of Suburbia, by: Greenday";
+        favSongs[1] = "Masterof Puppets, by: Metallica";
+        favSongs[2] = "The Trooper, by: Iron Maiden" ;
+        System.out.println("My favorite songs are:" + favSongs[0] + ", " + favSongs[1] + ", and " + favSongs[2]);
+       
+      
+    }
+        
+        }
+    
+    }
