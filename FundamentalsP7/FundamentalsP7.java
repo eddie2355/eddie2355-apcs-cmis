76d5a34d8fb482e5fcd71a2899a33ab1a9016d9a
cmis <cmis@cmis-hs20.(none)>
2015-08-25 11:55:39 +0700

    started P7

---
+++ FundamentalsP7/FundamentalsP7.java
@@ -0,0 +1,20 @@
+public class FundamentalsP7
+{
+   public static void main( String [] args)
+   {
+int[] randArray = new int[10];
+for(int index = 0; index < randArray.length; index++)
+{
+   randArray[ index ] = (int) (Math.random() * (201) - 100 ) ;
+  
+}
+while(  1 == 1)
+{
+   System.out.println( randArray[ index] ) ; 
+
+    }   
+    
+    
+      
+    
+}
