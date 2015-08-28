f0ea630ffaa950cf6bd41929579105366141140b
cmis <cmis@cmis-hs20.(none)>
2015-08-28 09:58:58 +0700

    worked on P9

--- Fundamentals9/FundamentalsP9.java
+++ Fundamentals9/FundamentalsP9.java
@@ -5,11 +5,26 @@ public class FundamentalsP9
    
     public static void main ( String[] args )
     {
+        String username = "Awesome";
+        int password = 1997 ;
         
-        System.out.println( "Please enter a username" );
+        System.out.println( "Username: " );
+        Scanner input1 = new Scanner( System.in );
+        String user = input1.nextLine();
+      
         
-        Scanner input = new Scanner( System.in );
-        String statement = input.nextLine();
+        System.out.println( "Password: " );
+        Scanner input2 = new Scanner( System.in );
+        int pass = input2.nextInt();
+        
+        if( user == username)
+        {
+            System.out.println("Login Successful");
+        }
+        else
+        {
+            System.out.println("Login Unsuccessful");
+        }
      
     }
 }
