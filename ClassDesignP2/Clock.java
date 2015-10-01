509293445d65564bb63a608c00367a27b12583eb
cmis <cmis@cmis-hs20.(none)>
2015-10-01 11:27:56 +0700

    finished class design p2

---
+++ ClassDesignP2/Clock.java
@@ -0,0 +1,28 @@
+public class Clock
+{
+    private int hour;
+    private int minute;
+    private int second;
+    
+    public Clock()
+        {
+            hour = 12;
+            minute = 34;
+            second = 56;
+        }
+    
+    public Clock(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+    
+    public String toString()
+        {
+            String output = String.format("The time is: %02d:%02d:%02d", hour, minute, second);
+            return output;
+        }
+        
+    
+}
---
+++ ClassDesignP2/ClockDriver.java
@@ -0,0 +1,11 @@
+public class ClockDriver
+{
+    public static void main ( String[] args )
+    {
+       Clock kitchenClock = new Clock();
+       Clock bedroomClock = new Clock(17, 1, 22);
+       
+       System.out.println( kitchenClock );
+       System.out.println( bedroomClock );
+    }
+}
