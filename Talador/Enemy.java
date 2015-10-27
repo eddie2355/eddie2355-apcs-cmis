fd0bd15a7186fe238b69e528839caaddfd2d133d
cmis <cmis@cmis-hs20.(none)>
2015-10-27 11:49:57 +0700

    talador work

---
+++ Talador/Enemy.java
@@ -0,0 +1,8 @@
+public class Enemy
+{
+    private int health;
+    private int dmg;
+    private String name;
+    //private String attack;
+   // public Enemy(int dmg 
+}
--- Talador/Room.java
+++ Talador/Room.java
@@ -11,12 +11,21 @@ public class Room
         this.direction = new String( direction );
         System.out.println("Looks like a dead end. Weird.");
     } //dead end room object + exit
-    public Room(String hall1)
+    public Room(String hall1, String goBack, String direction)
     {
         this.hall1 = new String( hall1 );
         this.goBack = new String( goBack );
-        thisdirection = new String( direction );
+        this.direction = new String( direction );
         System.out.println("Look there is a hallway! \n" +
                            "What shall we do");
     }//room with one hallway + exit
+    public Room(String hall1, String hall2, String direction, String goBack)
+    {
+        this.hall1 = new String( hall1 );
+        this.hall2 = new String( hall2 );
+        this.goBack = new String( goBack );
+        this.direction = new String( direction );
+        System.out.println("Look there are two hallways! \n" +
+                           "What shall we do");
+    }
 }
