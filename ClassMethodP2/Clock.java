d9880e3bda1772f8b4e424810b8aed5744566a70
cmis <cmis@cmis-hs20.(none)>
2015-11-09 09:51:31 +0700

    finished method p2

--- ClassMethodP2/Clock.java
+++ ClassMethodP2/Clock.java
@@ -3,29 +3,52 @@ public class Clock
     private int hour;
     private int minute;
     private int second;
-    
+    private String saveOrNot;
     public Clock()
         {
             hour = 12;
             minute = 34;
             second = 56;
+            saveOrNot = "springing forward";
         }
     
-    public Clock(int hour, int minute, int second)
+    public Clock(int hour, int minute, int second, String saveOrNot)
         {
             this.hour = hour;
             this.minute = minute;
             this.second = second;
+            this.saveOrNot = new String( saveOrNot );
         }
     
-    public void setTime()
+    public void setTime(int hour, int minute, int second)
         {
-           // String time = new String( hour + ":" + minute + ":" + second);
+           this.hour = hour;
+           this.minute = minute;
+           this.second = second;
+        }
+    
+    public void convertDayLightSaving()
+        {
+            if (saveOrNot.equals("falling back"))
+                {
+                    hour = hour - 1;
+                }
+            else if(saveOrNot.equals("springing forward"))
+                {
+                    hour = hour + 1;
+                }
+        }
+    
+    public int totalSeconds()
+        {
+            int seconds = 0;
+            seconds = (hour * 3600) + (minute * 60) + second;
+            return seconds;
         }
         
     public String toString()
         {
-            String output = String.format("The time is: %02d:%02d:%02d", hour, minute, second);
+            String output = String.format("The time is: %02d:%02d:%02d or in seconds its: " + totalSeconds(), hour, minute, second);
             return output;
         }
         
--- ClassMethodP2/ClockDriver.java
+++ ClassMethodP2/ClockDriver.java
@@ -3,11 +3,24 @@ public class ClockDriver
     public static void main ( String[] args )
     {
        Clock kitchenClock = new Clock();
-       Clock bedroomClock = new Clock(17, 1, 22);
+       Clock bedroomClock = new Clock(17, 1, 22, "falling back");
        
-       //System.out.println(kitchenClock.setTime());
-       
+       kitchenClock.setTime(11, 34, 56);
        System.out.println( kitchenClock );
+       System.out.println("Wait that can't be right. Did we take day light savings time into consideration?" + "\n"+
+                          "hmmmmmmmm, lets check.");
+       kitchenClock.convertDayLightSaving();
+       System.out.println( kitchenClock );
+       System.out.println("Nope we didn't. ( *glares at Phil* ) PHIL YOU HAD ONE JOB!!! GO FIX THE OTHER CLOCK \n\n");
+       
+       System.out.println("So did you fix it Phil? ... lets see.");
        System.out.println( bedroomClock );
+       System.out.println("PHIL DID YOU EVEN CHANGE IT???!!! PHIL STOP PLAYING GALAGA FOR PETE'S SAKE");
+       System.out.println("Pete: PLEASE PHIL!!!!!");
+       bedroomClock.convertDayLightSaving();
+       System.out.println( bedroomClock );
+       System.out.println("You are SOOOO( * dramatic pause * ) FIRED PHIL!\n\n");
+       
+       
     }
 }
