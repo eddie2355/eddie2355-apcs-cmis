e5309ebadca3578437c9e980c6f4f3a44d1c93e3
cmis <cmis@cmis-hs12.(none)>
2016-01-29 09:54:44 +0700

    started the big one

---
+++ ClassCompositionTheBigOne/GPS.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class GPS
+{
+    private ArrayList<Integer> markers = new ArrayList<Integer>();
+    
+    public GPS()
+    {
+        markers.add(100, 150);//0
+        markers.add(150);//1
+        markers.add(105);//2
+        markers.add(120);//3
+        markers.add(90);//4
+        markers.add(80);//5
+        markers.add(50);//6
+        markers.add(75);//7
+        markers.add(75);//8
+        markers.add(70);//9
+        markers.add(80);//10
+        markers.add(90);//11
+        markers.add(100);//12
+    }
+    
+}
---
+++ ClassCompositionTheBigOne/GPSApp.java
@@ -0,0 +1,9 @@
+public class GPSApp
+{
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
