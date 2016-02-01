db9707b529ca19a22083b079d4572a2cce6603ca
cmis <cmis@cmis-hs20.(none)>
2016-02-01 11:58:08 +0700

    worked on the big one

--- ClassCompositionTheBigOne/GPS.java
+++ ClassCompositionTheBigOne/GPS.java
@@ -5,7 +5,7 @@ public class GPS
     
     public GPS()
     {
-        markers.add(100, 150);//0
+        markers.add(100);//0
         markers.add(150);//1
         markers.add(105);//2
         markers.add(120);//3
@@ -20,4 +20,69 @@ public class GPS
         markers.add(100);//12
     }
     
+    public GPS( ArrayList<Integer> markers )
+    {
+        this.markers = markers;
+    }
+    
+    public void addMarker(int marker)
+    {
+        markers.add(marker);
+    }
+    
+    public int getLength()
+    {
+        return markers.size();
+    }
+    
+    public boolean isLevelTrailSegment(int begin, int end)
+    {
+        boolean isLevel = true;
+        int startIndex = 0;
+        int endIndex = 0;
+        
+        if(begin != end)
+        {
+           isLevel = false; 
+        }
+        else
+        {
+            for(int i = 0; i < markers.size(); i++)
+            {
+                if( markers.get(i) == begin)
+                {
+                    startIndex = i;
+                }
+            }
+            
+            for(int i = 0; i < markers.size(); i++)
+            {
+                if( markers.get(i) == end)
+                {
+                    endIndex = i;
+                }
+            }
+            
+            for(int i = startIndex; i < endIndex + 1; i ++)
+            {
+                if(markers.get(i) - markers.get(i + 1) >= 10)
+                {
+                    isLevel = false;
+                }
+            }
+        }
+        return isLevel;
+    }
+    
+    public String toString()
+    {
+        String output = new String("Index Elevation \n");
+        int counter = 0;
+        for ( int s : markers)
+        {
+            output += counter + " " + s + "\n" ;
+            counter ++;
+        }
+        return output;
+    }
 }
--- ClassCompositionTheBigOne/GPSApp.java
+++ ClassCompositionTheBigOne/GPSApp.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
 public class GPSApp
 {
     
     public static void main(String[] args)
     {
+        ArrayList<Integer> markers = new ArrayList<Integer>();
+        markers.add(90);//0
+        markers.add(80);//1
+        markers.add(90);//2
+        markers.add(110);//3
+        markers.add(120);//4
+        markers.add(100);//5
+        markers.add(80);//6
+        markers.add(110);//7
+        markers.add(150);//8
+        markers.add(160);//9
+        markers.add(160);//10
+        markers.add(120);//11
+        markers.add(100);//12
         
+        GPS carGPS = new GPS ();
+        GPS vanGPS = new GPS ( markers );
+        
+        System.out.println(vanGPS);
+        if(vanGPS.isLevelTrailSegment(110, 160))
+        {
+            System.out.println("worked");
+        }
+        else
+        {
+            System.out.println("nah");
+        }
     }
     
 }
