2559b014289e659dd958cc453655cf472d7bc974
cmis <cmis@cmis-hs20.(none)>
2016-05-17 11:44:37 +0700

    add grid

--- TowerDefense/MyWorld.java
+++ TowerDefense/MyWorld.java
@@ -18,6 +18,13 @@ public class MyWorld extends World
         super(690, 480, 1);
         setBackground(new GreenfootImage("8_paths_simple.png")); 
         setPaintOrder(Bullet.class, MyWorld.class);
+        
+        GreenfootImage cell = new GreenfootImage(26, 26);//13 fits perfectly, need # to fit map
+        //cell.setColor(java.awt.Color.white);
+        //cell.fill();
+        cell.setColor(java.awt.Color.gray);
+        cell.drawRect(0, 0, 49, 49);
+        setBackground(cell);
     }
     
     private void addPlayer() 
